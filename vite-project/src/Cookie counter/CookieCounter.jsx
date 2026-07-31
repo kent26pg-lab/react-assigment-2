606c5a9f15fba24aa32423f3d311e170cf63759b
kent <kent.26pg@gmail.com>
2026-07-31 13:26:47 +0200

    added import meta for the two images

--- vite-project/src/Cookie counter/CookieCounter.jsx
+++ vite-project/src/Cookie counter/CookieCounter.jsx
@@ -16,13 +16,13 @@ function CookieCounter() {
     <div className={styles.cookieCounter}>
       <h1>{count}</h1>
       <img
-        src="/cookie.png"
+        src="{`${import.meta.env.BASE_URL}cookie.png"
         alt="Cookie"
         onClick={handleClick}
         className={styles.cookieButton}
       />
       <img 
-      src="/cookiemonster.png" 
+      src="{`${import.meta.env.BASE_URL}cookiemonster.png" 
       alt="the cookie monster"
       onClick={resetClick}
       className={resetClick}
