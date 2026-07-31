35eab973cede9b91741e8962053f03f03369d787
kent <kent.26pg@gmail.com>
2026-07-31 12:39:19 +0200

    added CookieCounter function and gave it styling

---
+++ vite-project/src/Cookie counter/CookieCounter.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import styles from './CookieCounter.module.css';
+
+function CookieCounter() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  function resetClick() {
+    setCount(0);
+  }
+
+  return (
+    <div className={styles.cookieCounter}>
+      <h1>{count}</h1>
+      <img
+        src="/cookie.png"
+        alt="Cookie"
+        onClick={handleClick}
+        className={styles.cookieButton}
+      />
+      <img 
+      src="/cookiemonster.png" 
+      alt="the cookie monster"
+      onClick={resetClick}
+      className={resetClick}
+      />
+    </div>
+  );
+}
+
+export default CookieCounter;
