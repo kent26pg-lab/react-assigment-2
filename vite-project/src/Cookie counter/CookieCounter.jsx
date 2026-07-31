a107d520ba61b4ca32f6d65b2f2cee322f039294
kent <kent.26pg@gmail.com>
2026-07-31 14:56:02 +0200

    added speech bubbles to the cookie monster

--- vite-project/src/Cookie counter/CookieCounter.jsx
+++ vite-project/src/Cookie counter/CookieCounter.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import styles from './CookieCounter.module.css';
+import { useState } from "react";
+import styles from "./CookieCounter.module.css";
 
 function CookieCounter() {
   const [count, setCount] = useState(0);
@@ -10,22 +10,37 @@ function CookieCounter() {
 
   function resetClick() {
     setCount(0);
+    setLastCount(count);
+    setShowBubble(true);
   }
 
+  const [showBubble, setShowBubble] = useState(false);
+  const [lastCount, setLastCount] = useState(0);
   return (
     <div className={styles.cookieCounter}>
       <h1>{count}</h1>
       <img
-        src={`${import.meta.env.BASE_URL}cookie.png`} 
+        src={`${import.meta.env.BASE_URL}cookie.png`}
         alt="Cookie"
         onClick={handleClick}
         className={styles.cookieButton}
       />
-      <img 
-      src={`${import.meta.env.BASE_URL}cookiemonster.png`} 
-      alt="the cookie monster"
-      onClick={resetClick}
-      className={styles.cookieResetButton}
+
+      {!showBubble && (
+        <div className={styles.speechBubble}>Kan jeg få {count}? </div>
+      )}
+
+      {showBubble && (
+        <div className={styles.speechBubble}>
+          Tusen takk for {lastCount} kjeks!
+        </div>
+      )}
+
+      <img
+        src={`${import.meta.env.BASE_URL}cookiemonster.png`}
+        alt="the cookie monster"
+        onClick={resetClick}
+        className={styles.cookieResetButton}
       />
     </div>
   );
